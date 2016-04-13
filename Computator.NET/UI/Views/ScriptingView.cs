934615ef54ea3f9c9ba450a198a47fb84966b7e3
PTroka <pawel.troka@outlook.com>
2016-04-13 19:34:34 +0200

    ScriptingView MVP WIP

---
+++ Computator.NET/UI/Views/ScriptingView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Computator.NET.UI.Views
+{
+    public partial class ScriptingView : UserControl
+    {
+        public ScriptingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
