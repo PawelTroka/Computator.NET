807bc3c044afde46155cc93cfde19cfd05830919
PTroka <pawel.troka@outlook.com>
2016-12-30 02:01:18 +0100

    Reverted projects to .NET 4.6

--- Computator.NET.Core/Properties/Resources.Designer.cs
+++ Computator.NET.Core/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace Computator.NET.Core.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
