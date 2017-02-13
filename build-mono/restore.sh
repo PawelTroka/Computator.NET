8e23e45ea8a1788562b8fb5b1f84e63da92bc4aa
Pawel Troka <Pawel.Troka@powel.no>
2017-02-13 23:23:48 +0100

    restore.sh reverted

--- build-mono/restore.sh
+++ build-mono/restore.sh
@@ -2,8 +2,8 @@
 
 #if [ "$TRAVIS_OS_NAME" == "osx" ]; then brew update; brew install openssl; ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/; ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/; fiopenssl; fi
 
-export DISPLAY=99.0
-
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then "export DISPLAY=:99.0"; fi
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then sh -e /etc/init.d/xvfb start; fi
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then sh -e /etc/init.d/xvfb start; fi
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then sleep 3; fi # give xvfb some time to start
 
