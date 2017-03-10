99d950279815ae6043fb990aca5947ce369392ec
Pawel Troka <Pawel.Troka@powel.no>
2017-03-11 00:33:19 +0100

    Minor code improvement

--- Computator.NET.DataTypes/Configuration/GslConfig.cs
+++ Computator.NET.DataTypes/Configuration/GslConfig.cs
@@ -5,15 +5,9 @@ namespace Computator.NET.DataTypes
 {
     public static class GslConfig
     {
-        private static readonly AssemblyName An = Assembly.GetExecutingAssembly().GetName();
-    
         public static readonly string Location = RuntimeInformation.IsUnix
-            ? PathUtility.GetFullPath()//hack - for Unix we just copy .so files into app directory because otherwise it doesn't seem to work. On Windows we must use temp directory because we don't have admin rights.
+            ? AppInformation.Directory//hack - for Unix we just copy .so files into app directory because otherwise it doesn't seem to work. On Windows we must use temp directory because we don't have admin rights.
             : AppInformation.TempDirectory;
-
-
-
-
 
         public const string GslDllName = "gsl.dll";
 
@@ -27,7 +21,6 @@ namespace Computator.NET.DataTypes
         {
             return $"{OSPrefix}{baseName}{OSExtension}";
         }
-
 
         public static readonly string GslLibraryName = GetOSSpecificLibraryName("gsl");
 
