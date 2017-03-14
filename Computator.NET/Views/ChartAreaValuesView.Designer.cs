b0ae651795a30fc13268e80ef461d6a7e4d46089
PTroka <pawel.troka@outlook.com>
2017-03-14 01:12:02 +0100

    Chart area values view should now look good on even on 175% DPI (issue #37)

--- Computator.NET/Views/ChartAreaValuesView.Designer.cs
+++ Computator.NET/Views/ChartAreaValuesView.Designer.cs
@@ -36,24 +36,22 @@ namespace Computator.NET.Views
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.clearChartButton = new System.Windows.Forms.Button();
             this.addToChartButton = new System.Windows.Forms.Button();
-            this.yNlabel = new Computator.NET.Controls.Field();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
             this.yNNumericUpDown = new Computator.NET.Controls.ScientificNumericUpDown();
-            this.y0label = new Computator.NET.Controls.Field();
-            this.y0NumericUpDown = new Computator.NET.Controls.ScientificNumericUpDown();
-            this.label4 = new Computator.NET.Controls.Field();
-            this.xnNumericUpDown = new Computator.NET.Controls.ScientificNumericUpDown();
-            this.label3 = new Computator.NET.Controls.Field();
+            this.label3 = new System.Windows.Forms.Label();
             this.x0NumericUpDown = new Computator.NET.Controls.ScientificNumericUpDown();
+            this.y0label = new System.Windows.Forms.Label();
+            this.y0NumericUpDown = new Computator.NET.Controls.ScientificNumericUpDown();
+            this.yNlabel = new System.Windows.Forms.Label();
+            this.xnNumericUpDown = new Computator.NET.Controls.ScientificNumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.groupBox2.SuspendLayout();
-            this.yNlabel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.yNNumericUpDown)).BeginInit();
-            this.y0label.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.y0NumericUpDown)).BeginInit();
-            this.label4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.xnNumericUpDown)).BeginInit();
-            this.label3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.x0NumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y0NumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xnNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
@@ -76,10 +74,7 @@ namespace Computator.NET.Views
             // groupBox2
             // 
             resources.ApplyResources(this.groupBox2, "groupBox2");
-            this.groupBox2.Controls.Add(this.yNlabel);
-            this.groupBox2.Controls.Add(this.y0label);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tableLayoutPanel1);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.TabStop = false;
             // 
@@ -95,11 +90,23 @@ namespace Computator.NET.Views
             this.addToChartButton.Name = "addToChartButton";
             this.addToChartButton.UseVisualStyleBackColor = true;
             // 
-            // yNlabel
+            // tableLayoutPanel1
             // 
-            resources.ApplyResources(this.yNlabel, "yNlabel");
-            this.yNlabel.Controls.Add(this.yNNumericUpDown);
-            this.yNlabel.Name = "yNlabel";
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.yNNumericUpDown, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.x0NumericUpDown, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.y0label, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.y0NumericUpDown, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.yNlabel, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.xnNumericUpDown, 1, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
             // 
             // yNNumericUpDown
             // 
@@ -131,82 +138,9 @@ namespace Computator.NET.Views
             0,
             0});
             // 
-            // y0label
-            // 
-            resources.ApplyResources(this.y0label, "y0label");
-            this.y0label.Controls.Add(this.y0NumericUpDown);
-            this.y0label.Name = "y0label";
-            // 
-            // y0NumericUpDown
-            // 
-            resources.ApplyResources(this.y0NumericUpDown, "y0NumericUpDown");
-            this.y0NumericUpDown.Epsilon = new decimal(new int[] {
-            1,
-            0,
-            0,
-            196608});
-            this.y0NumericUpDown.Increment = new decimal(new int[] {
-            9,
-            0,
-            0,
-            65536});
-            this.y0NumericUpDown.Maximum = new decimal(new int[] {
-            -1,
-            -1,
-            -1,
-            65536});
-            this.y0NumericUpDown.Minimum = new decimal(new int[] {
-            -1,
-            -1,
-            -1,
-            -2147418112});
-            this.y0NumericUpDown.Name = "y0NumericUpDown";
-            this.y0NumericUpDown.Value = new decimal(new int[] {
-            3,
-            0,
-            0,
-            -2147483648});
-            // 
-            // label4
-            // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Controls.Add(this.xnNumericUpDown);
-            this.label4.Name = "label4";
-            // 
-            // xnNumericUpDown
-            // 
-            resources.ApplyResources(this.xnNumericUpDown, "xnNumericUpDown");
-            this.xnNumericUpDown.Epsilon = new decimal(new int[] {
-            1,
-            0,
-            0,
-            196608});
-            this.xnNumericUpDown.Increment = new decimal(new int[] {
-            20,
-            0,
-            0,
-            65536});
-            this.xnNumericUpDown.Maximum = new decimal(new int[] {
-            -1,
-            -1,
-            -1,
-            65536});
-            this.xnNumericUpDown.Minimum = new decimal(new int[] {
-            -1,
-            -1,
-            -1,
-            -2147418112});
-            this.xnNumericUpDown.Name = "xnNumericUpDown";
-            this.xnNumericUpDown.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
-            // 
             // label3
             // 
             resources.ApplyResources(this.label3, "label3");
-            this.label3.Controls.Add(this.x0NumericUpDown);
             this.label3.Name = "label3";
             // 
             // x0NumericUpDown
@@ -239,31 +173,95 @@ namespace Computator.NET.Views
             0,
             -2147483648});
             // 
+            // y0label
+            // 
+            resources.ApplyResources(this.y0label, "y0label");
+            this.y0label.Name = "y0label";
+            // 
+            // y0NumericUpDown
+            // 
+            resources.ApplyResources(this.y0NumericUpDown, "y0NumericUpDown");
+            this.y0NumericUpDown.Epsilon = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.y0NumericUpDown.Increment = new decimal(new int[] {
+            9,
+            0,
+            0,
+            65536});
+            this.y0NumericUpDown.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            65536});
+            this.y0NumericUpDown.Minimum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            -2147418112});
+            this.y0NumericUpDown.Name = "y0NumericUpDown";
+            this.y0NumericUpDown.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            -2147483648});
+            // 
+            // yNlabel
+            // 
+            resources.ApplyResources(this.yNlabel, "yNlabel");
+            this.yNlabel.Name = "yNlabel";
+            // 
+            // xnNumericUpDown
+            // 
+            resources.ApplyResources(this.xnNumericUpDown, "xnNumericUpDown");
+            this.xnNumericUpDown.Epsilon = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.xnNumericUpDown.Increment = new decimal(new int[] {
+            20,
+            0,
+            0,
+            65536});
+            this.xnNumericUpDown.Maximum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            65536});
+            this.xnNumericUpDown.Minimum = new decimal(new int[] {
+            -1,
+            -1,
+            -1,
+            -2147418112});
+            this.xnNumericUpDown.Name = "xnNumericUpDown";
+            this.xnNumericUpDown.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
             // ChartAreaValuesView
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.clearChartButton);
             this.Controls.Add(this.addToChartButton);
-            resources.ApplyResources(this, "$this");
             this.Name = "ChartAreaValuesView";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            this.yNlabel.ResumeLayout(false);
-            this.yNlabel.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.yNNumericUpDown)).EndInit();
-            this.y0label.ResumeLayout(false);
-            this.y0label.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.y0NumericUpDown)).EndInit();
-            this.label4.ResumeLayout(false);
-            this.label4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.xnNumericUpDown)).EndInit();
-            this.label3.ResumeLayout(false);
-            this.label3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.x0NumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y0NumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xnNumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -275,13 +273,14 @@ namespace Computator.NET.Views
         private System.Windows.Forms.GroupBox groupBox2;
         private ScientificNumericUpDown yNNumericUpDown;
         private ScientificNumericUpDown y0NumericUpDown;
-        private Field yNlabel;
-        private Field y0label;
+        private System.Windows.Forms.Label yNlabel;
+        private System.Windows.Forms.Label y0label;
         private ScientificNumericUpDown xnNumericUpDown;
         private ScientificNumericUpDown x0NumericUpDown;
-        private Field label4;
-        private Field label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button clearChartButton;
         private System.Windows.Forms.Button addToChartButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
