3d2252e91d3702001465310c499d2d71226685e7
Pawel Troka <Pawel.Troka@powel.no>
2017-06-01 14:10:26 +0200

    An attempt to compile new csproj on Mono build in Travis CI

--- build-mono/build.sh
+++ build-mono/build.sh
@@ -1,8 +1,5 @@
 if [ -z "$build_config" ]; then export build_config="Release"; fi #default is Release
 
-xbuild /p:Configuration="$build_config" Computator.NET.DataTypes/Computator.NET.DataTypes"$netmoniker".csproj
-xbuild /p:Configuration="$build_config" Computator.NET.Charting/Computator.NET.Charting"$netmoniker".csproj
-xbuild /p:Configuration="$build_config" Computator.NET.Core/Computator.NET.Core"$netmoniker".csproj
-xbuild /p:Configuration="$build_config" Computator.NET/Computator.NET"$netmoniker".csproj
-xbuild /p:Configuration="$build_config" Computator.NET.Tests/Computator.NET.Tests.csproj
-xbuild /p:Configuration="$build_config" Computator.NET.IntegrationTests/Computator.NET.IntegrationTests.csproj
+msbuild /p:Configuration="$build_config" Computator.NET/Computator.NET"$netmoniker".csproj
+msbuild /p:Configuration="$build_config" Computator.NET.Tests/Computator.NET.Tests.csproj
+msbuild /p:Configuration="$build_config" Computator.NET.IntegrationTests/Computator.NET.IntegrationTests.csproj
