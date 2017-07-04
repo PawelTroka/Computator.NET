35474fa3c69b8dfe869e622d69fbd17434f0f395
Pawel Troka <Pawel.Troka@powel.no>
2017-07-04 16:56:20 +0200

    Cleaned up a bit new installer code

--- Computator.NET.Setup/BootstrapperBuilder.cs
+++ Computator.NET.Setup/BootstrapperBuilder.cs
@@ -7,42 +7,38 @@ namespace Computator.NET.Setup
 {
     public class BootstrapperBuilder
     {
-
         public void Build()
         {
             var projectBuilder = new ProjectBuilder("4.6.1");
             var projectBuilderNet40 = new ProjectBuilder("4.0");
 
+            Console.WriteLine($"Building {nameof(MsiPackage)}s...");
             var productMsi = projectBuilder.BuildMsi();
             var productMsiNet40 = projectBuilderNet40.BuildMsi();
-
-
             var productMsiPackage = new MsiPackage(productMsi) {DisplayInternalUI = true, InstallCondition = "VersionNT >= v6.0" };
             var productMsiPackageNet40 = new MsiPackage(productMsiNet40) { Id = "Computator.NET__Windows_XP", DisplayInternalUI = true, InstallCondition = "VersionNT < v6.0" };
 
-            var packegeGroupRefNet40 = PrerequisiteHelper.GetPackegeRef(projectBuilderNet40.CurrentHighestVersion);
-            var packegeGroupRefNet = PrerequisiteHelper.GetPackegeRef(projectBuilder.CurrentHighestVersion);
+            Console.WriteLine($"Building {nameof(PackageGroupRefWrapper)}s...");
+            var packageGroupRefWrapperNet40 = new PackageGroupRefWrapper(projectBuilderNet40.CurrentHighestVersion);
+            var packageGroupRefWrapperNet = new PackageGroupRefWrapper(projectBuilder.CurrentHighestVersion);
+            var packegeGroupRefNet40Path = packageGroupRefWrapperNet40.Build();
+            var packegeGroupRefNetPath = packageGroupRefWrapperNet.Build();
 
-            var packegeGroupRefPathNet40 = Path.Combine(SharedProperties.OutDir, $"{packegeGroupRefNet40}.exe");
-            var packegeGroupRefNetPath = Path.Combine(SharedProperties.OutDir, $"{packegeGroupRefNet}.exe");
-
-            (new Bundle(packegeGroupRefNet40,new PackageGroupRef(packegeGroupRefNet))
-            {
-                Version = projectBuilderNet40.CurrentHighestVersion.RealVersion,
-                //Application = new SilentBootstrapperApplication(),
-            }).Build(packegeGroupRefPathNet40);
-            (new Bundle(packegeGroupRefNet, new PackageGroupRef(packegeGroupRefNet))
-            {
-                Version = projectBuilder.CurrentHighestVersion.RealVersion,
-                //Application = new SilentBootstrapperApplication(),
-            }).Build(packegeGroupRefNetPath);
-
-
-
+            Console.WriteLine($"Creating final {nameof(Bundle)} object");
             var bootstrapper =
                 new Bundle("Computator.NET",
-                    new ExePackage(packegeGroupRefPathNet40) { InstallCondition = "VersionNT < v6.0", InstallCommand = "-q",Compressed = true},
-                    new ExePackage(packegeGroupRefNetPath) { InstallCondition = "VersionNT >= v6.0", InstallCommand = "-q", Compressed = true},
+                    new ExePackage(packegeGroupRefNet40Path)
+                    {
+                        InstallCondition = "VersionNT < v6.0",
+                        InstallCommand = "-q",
+                        Compressed = true
+                    },
+                    new ExePackage(packegeGroupRefNetPath)
+                    {
+                        InstallCondition = "VersionNT >= v6.0",
+                        InstallCommand = "-q",
+                        Compressed = true
+                    },
                     productMsiPackageNet40,
                     productMsiPackage)
                 {
@@ -62,18 +58,17 @@ namespace Computator.NET.Setup
                     }*/
                     Application = new LicenseBootstrapperApplication()
                     {
-                        LogoFile = @"../Graphics/computator.net-icon.png",//SharedProperties.LogoBmp,
-                        LicensePath = SharedProperties.License,//@"https://github.com/PawelTroka/Computator.NET/blob/master/LICENSE"
-                    }
+                        LogoFile = @"../Graphics/computator.net-icon.png", //SharedProperties.LogoBmp,
+                        LicensePath = SharedProperties.License, //@"https://github.com/PawelTroka/Computator.NET/blob/master/LICENSE"
+                    },
+                    SplashScreenSource = SharedProperties.LogoBmp,//@"..\Graphics\computator.net-icon.png",//@"..\Graphics\Installer\InstallShield Computator.NET Theme\setup.gif",
+                    //PreserveTempFiles = true,
                 };
 
             Console.WriteLine($"Adding {nameof(PatchKnowledgeBase2468871)} for async-await support on Windows XP.");
             var patchKnowledgeBase2468871 = new PatchKnowledgeBase2468871();
             var patchesForNet40 = patchKnowledgeBase2468871.Build(bootstrapper);
             bootstrapper.Chain.InsertRange(1, patchesForNet40);
-
-            bootstrapper.SplashScreenSource = SharedProperties.LogoBmp;//@"..\Graphics\computator.net-icon.png";//@"..\Graphics\Installer\InstallShield Computator.NET Theme\setup.gif";          
-            //bootstrapper.PreserveTempFiles = true;
 
             var finalPath = Path.Combine(SharedProperties.OutDir, "Computator.NET.Setup.exe");
             Console.WriteLine($"Building final bundle '{finalPath}'");
---
+++ Computator.NET.Setup/PackageGroupRefWrapper.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using WixSharp.Bootstrapper;
+
+namespace Computator.NET.Setup
+{
+    /// <summary>
+    /// Wraps PackageGroupRef in Exe Bundle so it can be used in final bootstrapper along with other PackageGroupRefs
+    /// Inspired by: https://stackoverflow.com/a/24557280/2583080
+    /// </summary>
+    internal class PackageGroupRefWrapper
+    {
+        private readonly NetVersion _netVersion;
+
+        public PackageGroupRefWrapper(NetVersion netVersion)
+        {
+            _netVersion = netVersion;
+        }
+
+        public string Build()
+        {
+            var packegeGroupRef = PrerequisiteHelper.GetPackegeRef(_netVersion);
+            var packegeGroupRefPath = Path.Combine(SharedProperties.OutDir, $"{packegeGroupRef}.exe");
+            
+            var wrapperBundle = new Bundle(packegeGroupRef, new PackageGroupRef(packegeGroupRef))
+            {
+                Version = _netVersion.RealVersion,
+                //Application = new SilentBootstrapperApplication(),
+            };
+
+            Console.WriteLine($"Building {nameof(PackageGroupRefWrapper)} for {nameof(PackageGroupRef)} '{packegeGroupRef}' for .NET version '{_netVersion}'");
+            var wrapperPath = wrapperBundle.Build(packegeGroupRefPath);
+            Console.WriteLine($"Build succesful, path is '{wrapperPath}'");
+
+            return wrapperPath;
+        }
+    }
+}
