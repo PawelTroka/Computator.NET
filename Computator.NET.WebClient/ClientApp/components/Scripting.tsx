0f14cab65d53615b41c050a69ccbf9708123faf6
Pawel Troka <Pawel.Troka@powel.no>
2017-08-02 19:14:29 +0200

    web frontend: implemented simple scripting view

---
+++ Computator.NET.WebClient/ClientApp/components/Scripting.tsx
@@ -0,0 +1,87 @@
+import * as Config from '../config';
+import * as React from "react";
+import "isomorphic-fetch";
+import "dompurify";
+
+interface IScriptState {
+    expression: string;
+    result: string;
+}
+
+export class Scripting extends React.Component<{}, IScriptState>
+{
+    public constructor()
+    {
+        super();
+        this.state = { result: "", expression: "" };
+    }
+
+    public render(): JSX.Element
+    {
+        var outputStyle = {
+            whiteSpace: "pre-wrap"
+        };
+        return <div>
+            <h1>Scripting</h1>
+            <p>Write down script in TSL</p>
+            <br />
+
+            <div className="row">
+                <div className="col-md-9">
+                    <div className="form-group">
+                        <label htmlFor="scriptTextArea">Script:</label>
+                        <textarea className="form-control" id="scriptTextArea" defaultValue={this.state.expression} onChange={e => this.handleChange(e)} rows={14} cols={45} />
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <button className="btn btn-primary btn-lg pull-right" onClick={e => this.handleSubmit(e)}>Process<br />script</button>
+                </div>
+            </div>
+            <br />
+               <div className="row">
+            <div className="col-md-12">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Output: </h3>
+                        </div>
+                        <div className="panel-body">
+                            <strong style={outputStyle}>
+                                {this.state.result}
+                            </strong>
+                        </div>
+                    </div>
+            </div></div>
+        </div>;
+    }
+
+    private handleChange(event: React.ChangeEvent<HTMLTextAreaElement>): void
+    {
+        const newValue = event.target.value;
+        if (newValue != null)
+        {
+            console.log(`Changing script to ${newValue}`);
+            this.setState(prevState => prevState.expression = newValue);
+        }
+
+    }
+
+    private handleSubmit(event : React.MouseEvent<HTMLButtonElement>) : void
+    {
+        event.preventDefault();
+        if (this.state.expression != null && this.state.expression !== "")
+            this.process(this.state.expression);
+    }
+
+    private process(expression : string): void
+    {
+        const apiUrl = `${Config.WEB_API_BASE_URL}/script/${encodeURIComponent(expression)}`;
+        console.log(`Calling ${apiUrl}`);
+
+        fetch(apiUrl)
+            .then(response => response.text() as Promise<string>)
+            .then(data => {
+                console.log(`Got result: ${data}`);
+                this.setState(prevState => prevState.result = data);
+            });
+    }
+}
--- Computator.NET.WebClient/ClientApp/routes.tsx
+++ Computator.NET.WebClient/ClientApp/routes.tsx
@@ -4,9 +4,11 @@ import { Layout } from './components/Layout';
 import { FetchData } from './components/FetchData';
 import { Calculate } from './components/Calculate';
 import { Chart } from './components/Chart';
+import { Scripting } from './components/Scripting';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Chart } />
-    <Route path='/calculate' component={ Calculate } />
+    <Route path='/calculate' component={Calculate} />
+    <Route path='/scripting' component={Scripting} />
     <Route path='/fetchdata' component={ FetchData } />
 </Layout>;
