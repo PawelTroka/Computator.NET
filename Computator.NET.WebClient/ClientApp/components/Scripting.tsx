42e58ad4f8827a6779e339f5ef90a7eb9d68b36c
Pawel Troka <Pawel.Troka@powel.no>
2017-08-03 13:26:04 +0200

    Web frontend: scripting view is now using ace component

--- Computator.NET.WebClient/ClientApp/components/Scripting.tsx
+++ Computator.NET.WebClient/ClientApp/components/Scripting.tsx
@@ -1,6 +1,15 @@
 import * as Config from '../config';
 import * as React from "react";
 import "isomorphic-fetch";
+import AceEditor from "react-ace";
+import * as brace from 'brace';
+
+
+import 'brace/mode/csharp';
+import 'brace/snippets/csharp';
+import 'brace/theme/ambiance';
+import 'brace/ext/searchbox';
+import 'brace/ext/language_tools';
 
 interface IScriptState {
     expression: string;
@@ -20,6 +29,12 @@ export class Scripting extends React.Component<{}, IScriptState>
         const outputStyle = {
             whiteSpace: "pre-wrap"
         };
+        const editorStyle = {
+            border: "1px solid lightgray"
+        }
+        const editorPanelStyle = {
+            backgroundColor: "#3d3d3d"
+        }
         return <div>
             <h1>Scripting</h1>
             <p>Write down script in TSL</p>
@@ -27,13 +42,36 @@ export class Scripting extends React.Component<{}, IScriptState>
 
             <div className="row">
                 <div className="col-md-9">
-                    <div className="form-group">
-                        <label htmlFor="scriptTextArea">Script:</label>
-                        <textarea className="form-control" id="scriptTextArea" defaultValue={this.state.expression} onChange={e => this.handleChange(e)} rows={14} cols={45} />
-                    </div>
-                </div>
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Script: </h3>
+                        </div>
+                        <div className="panel-body" style={editorPanelStyle}>
+                                <AceEditor
+                        width="100%"
+                        height="100%"
+                        mode="csharp"
+                        minLines={15}
+                        maxLines={25}
+                        theme="ambiance"
+                        name="scriptTextArea"
+                        onChange={s => this.onChange(s)}
+                        fontSize={16}
+                        showPrintMargin={false}
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        value={this.state.expression}
+                        enableBasicAutocompletion={true}
+                        enableLiveAutocompletion={true}
+                        tabSize={4}
+                        setOptions={{
+                            enableSnippets: true,
+                            showLineNumbers: true,
+                            autoScrollEditorIntoView: true
+                        }} />
+                            </div></div></div>
                 <div className="col-md-3">
-                    <button className="btn btn-primary btn-lg pull-right" onClick={e => this.handleSubmit(e)}>Process<br />script</button>
+                    <button className="btn btn-primary btn-lg" onClick={e => this.handleSubmit(e)}>Process<br />script</button>
                 </div>
             </div>
             <br />
@@ -51,6 +89,14 @@ export class Scripting extends React.Component<{}, IScriptState>
                     </div>
             </div></div>
         </div>;
+    }
+
+    private onChange(newValue : string): void {
+        if (newValue != null) {
+            console.log(`Changing script to ${newValue}`);
+            this.setState(prevState => prevState.expression = newValue);
+        }
+
     }
 
     private handleChange(event: React.ChangeEvent<HTMLTextAreaElement>): void
