b34b370962098dca697946ff360e336b3a5009bd
Pawel Troka <Pawel.Troka@powel.no>
2017-08-18 16:08:06 +0200

    Web frontend: introduced autocomplete with fearture parity with scripting autocomplete

--- Computator.NET.WebClient/ClientApp/components/Expression.tsx
+++ Computator.NET.WebClient/ClientApp/components/Expression.tsx
@@ -2,17 +2,36 @@
 import * as React from "react";
 import "isomorphic-fetch";
 
+import AceEditor from "react-ace";
+import { CustomCompleter } from "../helpers/CustomCompleter";
+import * as brace from 'brace';
+
+
+import 'brace/mode/csharp';
+import 'brace/snippets/csharp';
+import 'brace/theme/github';
+import 'brace/ext/searchbox';
+import 'brace/ext/language_tools';
+import 'brace/ext/keybinding_menu'
+import 'brace/ext/settings_menu'
+
 interface IExpressionProps {
     expression: string;
     onExpressionChange: (expr: string) => void;
 }
 
+interface IExpressionState {
+    
+}
 
 export class Expression extends React.Component<IExpressionProps, {}>
 {
     public constructor(props: IExpressionProps) {
         super(props);
         //this.handleChange = this.handleChange.bind(this);
+        const langTools = brace.acequire("ace/ext/language_tools");
+        const customCompleter = new CustomCompleter("expression");
+        langTools.setCompleters([customCompleter]);
     }
 
     public componentDidMount(): void
@@ -23,8 +42,33 @@ export class Expression extends React.Component<IExpressionProps, {}>
     public render(): JSX.Element
     {
         return <div className="input-group input-group-lg">
-                   <span className="input-group-addon"><span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Expression:</span>
-                   <input type="text" defaultValue={this.props.expression} onChange={e => this.handleChange(e)} className="form-control" placeholder="here write expression, example: 2x-cos(x)" aria-describedby="expression" required />
+            <span className="input-group-addon"><span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> Expression:</span>
+            <AceEditor
+                className="form-control" aria-describedby="expression" 
+                editorProps={{
+                    $blockScrolling: Infinity
+                }}
+                       width="100%"
+                       height="100%"
+                       mode="csharp"
+                       minLines={1}
+                       maxLines={1}
+                       theme="github"
+                       name="expressionTextArea"
+                       onChange={s => this.props.onExpressionChange(s.replace('\n',"").replace('\r',""))}
+                       fontSize={36}
+                       showPrintMargin={false}
+                       showGutter={false}
+                       highlightActiveLine={false}
+                       value={this.props.expression}
+                       enableBasicAutocompletion={true}
+                       enableLiveAutocompletion={false}
+                       tabSize={4}
+                       setOptions={{
+                    enableSnippets: false,
+                    showLineNumbers: false,
+                    autoScrollEditorIntoView: true
+                }} />
                </div>;
     }
 
